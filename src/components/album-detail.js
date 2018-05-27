9881b3f8e1c318f843b188eba6baf6c1ee572e57
Alex Ferrer <aferrerdev@gmail.com>
2018-05-27 21:21:18 +0200

    Finishing album detail component.

--- src/components/album-detail.js
+++ src/components/album-detail.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import axios from 'axios';
 import Card from './card';
 import CardSection from './card-section';
 
 export default class AlbumDetail extends Component {
-
     render() {
         return (
             <Card>
                 <CardSection>
-                    <Text>{this.props.album.title}</Text>
+                    <View style={styles.thumbnailContainerStyle}>
+                        <Image
+                            style={styles.thumbnailStyle}
+                            source={{ uri: this.props.album.thumbnail_image }} 
+                        />
+                    </View>
+                    <View style={styles.headerContentStyle}>
+                        <Text style={styles.headerTextStyle}>{this.props.album.title}</Text>
+                        <Text>{this.props.album.artist}</Text>
+                    </View>    
                 </CardSection>
                 <CardSection>
-                    <Text>{this.props.album.title}</Text>
+                    <Image style={styles.imageStyle} source={{ uri: this.props.album.image }} />
                 </CardSection>
             </Card>
         );
     }
+}
 
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
 }
